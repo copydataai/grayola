a78f17e00962c72f052a740c9c5d022a8988017d
Jose Sanchez <engineer@copydataai.com>
2024-07-18 10:19:49 +0100

    feat(components): add Navbar

---
+++ apps/nextjs/src/app/_components/Navbar.tsx
@@ -0,0 +1,26 @@
+"use server";
+
+import Link from "next/link";
+import { Origami } from "lucide-react";
+
+import { Button } from "@acme/ui/button";
+
+export async function Navbar() {
+  return (
+    <nav className="flex w-1/2 items-center justify-between gap-4">
+      <div>
+        <Origami size={28} />
+      </div>
+      <div>
+        <Link href="/auth/login" asChild>
+          <Button
+            variant="outline"
+            className="border-x-1 border-t-1 border-b-4 border-x-black border-b-black border-t-black bg-gradient-to-r from-emerald-500 to-sky-500 hover:from-pink-500 hover:to-yellow-500"
+          >
+            Getting started
+          </Button>
+        </Link>
+      </div>
+    </nav>
+  );
+}
