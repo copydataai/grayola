578e62cebd1c14c01bb7118c944206f17e3621cd
Jose Sanchez <engineer@copydataai.com>
2024-07-18 23:19:32 +0100

    feat(nextjs): update main page

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,43 +1,52 @@
-import { Suspense } from "react";
+"use server";
+
+import Link from "next/link";
+
+import { Button } from "@acme/ui/button";
 
 import { api } from "~/trpc/server";
 import { AuthShowcase } from "./_components/auth-showcase";
-import {
-  CreatePostForm,
-  PostCardSkeleton,
-  PostList,
-} from "./_components/posts";
+import { Navbar } from "./_components/Navbar";
 
-export const runtime = "edge";
-
-export default function HomePage() {
+export default async function HomePage() {
   // You can await this here if you don't want to show Suspense fallback below
-  const posts = api.post.all();
 
   return (
-    <main className="container h-screen py-16">
-      <div className="flex flex-col items-center justify-center gap-4 py-8">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          <span className="text-fuchsia-500">T3</span> Turbo x{" "}
-          <span className="text-emerald-400">Supabase</span>
-        </h1>
-        <AuthShowcase />
-
-        <CreatePostForm />
-        <div className="w-full max-w-2xl overflow-y-scroll">
-          <Suspense
-            fallback={
-              <div className="flex w-full flex-col gap-4">
-                <PostCardSkeleton />
-                <PostCardSkeleton />
-                <PostCardSkeleton />
-              </div>
-            }
-          >
-            <PostList posts={posts} />
-          </Suspense>
+    <>
+      <header className="flex h-12 w-full items-center justify-center px-4">
+        <Navbar />
+      </header>
+      <main className="container flex h-screen flex-col items-center justify-center space-x-8 from-sky-500 to-emerald-500 py-16 md:flex-row">
+        <div className="flex flex-col items-center justify-center gap-4 py-8">
+          <h1 className="inline bg-gradient-to-b from-emerald-500 to-sky-500 bg-clip-text text-[2.5rem] font-semibold tracking-tight text-transparent lg:text-5xl">
+            Grayola
+          </h1>
+          <div className="">
+            <h2 className="text-xl font-bold text-sky-500 underline decoration-sky-500">
+              Your Design as a Service
+            </h2>
+            <h3 className="font-semibold text-emerald-600">
+              Get designs at least of 72 hours
+            </h3>
+          </div>
+          <Link href="/auth/signin" asChild>
+            <Button
+              variant="outline"
+              size="lg"
+              className="border-x-1 border-t-1 animate-pulse border-b-4 border-x-black border-b-black border-t-black bg-gradient-to-r from-emerald-500 to-sky-500 hover:from-pink-500 hover:to-yellow-500 hover:delay-1000 "
+            >
+              Free Trial
+            </Button>
+          </Link>
         </div>
-      </div>
-    </main>
+        <div>
+          <img
+            className="size-64 w-64 rounded-lg shadow-2xl"
+            src="/team-collaborating.webp"
+            alt="a full team working for you"
+          />
+        </div>
+      </main>
+    </>
   );
 }
