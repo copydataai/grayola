bb60469030c86a56a8a9e4fa41c17c5b89a6dd69
Jose Sanchez <engineer@copydataai.com>
2024-07-23 10:43:47 +0100

    feat(DialogFile): add Loader & open state

--- apps/nextjs/src/app/_components/DialogFile.tsx
+++ apps/nextjs/src/app/_components/DialogFile.tsx
@@ -24,6 +24,7 @@ import {
   FormMessage,
 } from "@acme/ui/form";
 import { Input } from "@acme/ui/input";
+import { LoaderCircleLucide } from "@acme/ui/loader-circle";
 import { toast } from "@acme/ui/sonner";
 import { Textarea } from "@acme/ui/textarea";
 import { CreateFile, CreateFileSchema } from "@acme/validators";
@@ -35,39 +36,49 @@ type DialogFileProps = {
   projectId: string;
 };
 
+const MAX_FILE_SIZE = 5000000;
+
 export function DialogFile(props: DialogFileProps) {
+  const [fileContent, setFileContent] = useState<File | null>(null);
+  const [fileName, setFileName] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
   const mutation = api.project.createFile.useMutation({
     onSuccess: () => {
       toast.success("File uploaded");
+      setIsOpen(false);
     },
     onError: (error) => {
       toast.error(error.message);
     },
   });
-  const [fileContent, setFileContent] = useState<File | null>(null);
-  const [fileName, setFileName] = useState<string>("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  const form = useForm({
-    schema: CreateFileSchema,
-  });
+  // TODO: add validation by zod
+  const form = useForm({});
   const handleFileChange = async ({ file }: { file: File | null }) => {
     try {
+      await setIsLoading(true);
       if (!file) throw new Error("Function: handleFileChange expects a file");
 
-      if (file.size > 2 * 1024 * 1024) {
-        throw new Error("File size must not exceed 2MB");
+      if (file.size > MAX_FILE_SIZE) {
+        throw new Error("File size must not exceed 5MB");
       }
       await setFileContent(file);
       await setFileName(file.name);
-    } catch (err) {
+    } catch (err: Error) {
       toast.error(err);
     }
+    await setIsLoading(false);
   };
 
-  async function onSubmit(values: CreateFile) {
+  async function onSubmit() {
+    if (fileContent === null) {
+      toast.error("Please select a file");
+      form.reset();
+      return;
+    }
     const { data, error } = await UploadFile(fileName, fileContent);
-    if (error) toast.error(error);
+    if (error) toast.error(`Error uploading ${fileName}file`);
     const newFile = await mutation.mutate({
       path: data.fullPath,
       projectId: props.projectId,
@@ -75,7 +86,7 @@ export function DialogFile(props: DialogFileProps) {
   }
 
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger asChild>
         <Button variant="outline">Add files</Button>
       </DialogTrigger>
@@ -87,7 +98,7 @@ export function DialogFile(props: DialogFileProps) {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
             <FormField
               control={form.control}
-              name="files"
+              name="file"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>File</FormLabel>
@@ -95,8 +106,9 @@ export function DialogFile(props: DialogFileProps) {
                     <Input
                       type="file"
                       onChange={(e) => {
-                        handleFileChange({ file: e.target?.files[0]! });
+                        handleFileChange({ file: e.target?.files[0] });
                       }}
+                      required
                     />
                   </FormControl>
                   <FormMessage />
@@ -104,11 +116,15 @@ export function DialogFile(props: DialogFileProps) {
               )}
             />
             <DialogFooter className="sm:justify-start">
-              <DialogClose asChild>
-                <Button variant="outline" type="submit" className="">
-                  Save changes
-                </Button>
-              </DialogClose>
+              <Button
+                variant="outline"
+                type="submit"
+                disabled={isLoading}
+                className="bg-emerald-500"
+              >
+                {isLoading && <LoaderCircleLucide className="mr-2 h-4 w-4" />}
+                Save changes
+              </Button>
             </DialogFooter>
           </form>
         </Form>
