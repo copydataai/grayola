40c03e2c1a7ef9c0c5e9fe9a5a2a33e949a43a33
Jose Sanchez <engineer@copydataai.com>
2024-07-23 09:54:10 +0100

    feat(project): add toast & detail loader

--- apps/nextjs/src/app/dashboard/project/[projectId]/page.tsx
+++ apps/nextjs/src/app/dashboard/project/[projectId]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { LoaderCircleLucide } from "@acme/ui/loader-circle";
+import { toast } from "@acme/ui/sonner";
 
 import { DialogFile } from "~/app/_components/DialogFile";
 import { api } from "~/trpc/react";
@@ -11,19 +12,31 @@ export default function ProjectIdPage({
   params: { projectId: string };
 }) {
   const { projectId } = params;
-  const { data, isLoading, isError, error } =
-    api.project.getFilesByProjectId.useQuery({
-      projectId,
-    });
+  const {
+    data = [],
+    isLoading,
+    isError,
+    error,
+  } = api.project.getFilesByProjectId.useQuery({
+    projectId,
+  });
   if (isLoading)
     return (
       <div className="flex h-screen w-screen items-center justify-center">
-        <LoaderCircleLucide />
+        <LoaderCircleLucide className="h-12 w-12" />
       </div>
     );
 
-  if (isError) return <div>Error: {error.message}</div>;
+  if (isError) {
+    toast.error(error.message);
+    return (
+      <div className="flex h-screen w-screen items-center justify-center">
+        <LoaderCircleLucide className="h-12 w-12 text-red-500" />
+      </div>
+    );
+  }
 
+  // TODO: add table to show and filter by created_at, updatedAt
   return (
     <main className="flex flex-col items-center justify-center gap-4 pt-4">
       <div>
