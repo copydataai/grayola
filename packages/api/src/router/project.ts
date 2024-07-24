12ba5c07cdf9f4b9ed989dabb3432fcf64009c35
Jose Sanchez <engineer@copydataai.com>
2024-07-24 16:10:07 +0100

    feat(api): add new endpoints

--- packages/api/src/router/project.ts
+++ packages/api/src/router/project.ts
@@ -1,18 +1,40 @@
 import type { TRPCRouterRecord } from "@trpc/server";
+import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import { TRPCError } from "@trpc/server";
 import { eq } from "drizzle-orm";
 import { z } from "zod";
 
-import { Files, Profile, Project, ProjectUsers, Role } from "@acme/db/schema";
+import {
+    Files,
+    Profile,
+    Project,
+    ProjectUsers,
+    Role,
+    Roles,
+} from "@acme/db/schema";
 import { CreateProjectSchema } from "@acme/validators";
 
 import { protectedProcedure, publicProcedure } from "../trpc";
 
-enum Roles {
-    Customer = "customer",
-    ProjectManager = "project manager",
-    Designer = "designer",
-    Admin = "admin",
-}
+const getRole = async (db: PostgresJsDatabase, projectId: string) => {
+    const project = await db
+        .select()
+        .from(ProjectUsers)
+        .where(eq(ProjectUsers.projectId, projectId));
+
+    if (project.length === 0) {
+        throw new TRPCError({
+            code: "NOT_FOUND",
+            message: "Project not found",
+        });
+    }
+    const role = await db
+        .select()
+        .from(Role)
+        .where(eq(Role.id, project[0]!.roleId));
+
+    return role[0]!;
+};
 
 export const projectRouter = {
     create: protectedProcedure
@@ -42,6 +64,48 @@ export const projectRouter = {
 
             return project;
         }),
+
+    listAllProfiles: protectedProcedure.query(async ({ ctx, input }) => {
+        // TODO: add a role identification
+        const profiles = await ctx.db.select().from(Profile);
+        return profiles;
+    }),
+    listRoles: protectedProcedure.query(async ({ ctx, input }) => {
+        const roles = await ctx.db.select().from(Role);
+        return roles;
+    }),
+
+    assignProject: protectedProcedure
+        .input(
+            z.object({
+                projectId: z.string(),
+                roleId: z.string(),
+                profileId: z.string(),
+            }),
+        )
+        .mutation(async ({ ctx, input }) => {
+            const role = await getRole(ctx.db, input.projectId);
+
+            if (role.name === Roles.Customer || role.name === Roles.Designer) {
+                throw new TRPCError({
+                    code: "FORBIDDEN",
+                    message: "Only project manager can assign roles",
+                });
+            }
+
+            console.log("input", input);
+
+            const assign = await ctx.db
+                .insert(ProjectUsers)
+                .values({
+                    roleId: input.roleId,
+                    projectId: input.projectId,
+                    profileId: input.profileId,
+                })
+                .returning();
+
+            return assign;
+        }),
     createFile: protectedProcedure
         .input(z.object({ path: z.string(), projectId: z.string() }))
         .mutation(async ({ ctx, input }) => {
@@ -51,6 +115,16 @@ export const projectRouter = {
                 .returning();
 
             return file;
+        }),
+    getRoleByProjectId: protectedProcedure
+        .input(
+            z.object({
+                projectId: z.string(),
+            }),
+        )
+        .query(async ({ ctx, input }) => {
+            const role = await getRole(ctx.db, input.projectId);
+            return role;
         }),
     getFilesByProjectId: protectedProcedure
         .input(z.object({ projectId: z.string() }))
@@ -81,16 +155,66 @@ export const projectRouter = {
 
         return projects;
     }),
+    listAllAndRole: protectedProcedure.query(async ({ ctx }) => {
+        const profileId = await ctx.user.id;
+
+        const projects = await ctx.db
+            .select({
+                id: Project.id,
+                name: Project.name,
+                description: Project.description,
+                customerId: Project.customerId,
+                createdAt: Project.createdAt,
+                updatedAt: Project.updatedAt,
+                role: Role.name,
+            })
+            .from(ProjectUsers)
+            .where(eq(ProjectUsers.profileId, profileId))
+            .innerJoin(Project, eq(Project.id, ProjectUsers.projectId))
+            .innerJoin(Role, eq(Role.id, ProjectUsers.roleId));
+
+        return projects;
+    }),
+    update: protectedProcedure
+        .input(
+            z.object({
+                projectId: z.string(),
+                name: z.string().optional(),
+                description: z.string().optional(),
+            }),
+        )
+        .mutation(async ({ ctx, input }) => {
+            const role = await getRole(ctx.db, input.projectId);
+            if (role.name !== Roles.ProjectManager) {
+                throw new TRPCError({
+                    code: "UNAUTHORIZED",
+                    message:
+                        "Only the project manager is allowed to update the project",
+                });
+            }
+
+            const project = await ctx.db
+                .update(Project)
+                .set({ ...input })
+                .where(eq(Project.id, input.projectId))
+                .returning();
+
+            return project;
+        }),
     delete: protectedProcedure
-        .input(z.object({ id: z.string() }))
+        .input(z.object({ projectId: z.string() }))
         .query(async ({ ctx, input }) => {
-            // TODO: just project that users belongs
-            // TODO: check if user is project manager
-            //       if (data?.authorId !== ctx.user.id) {
-            // throw new TRPCError({
-            //   code: "UNAUTHORIZED",
-            //   message: "Only the project manager is allowed to delete the project",
-            // });
-            // }
+            const role = await getRole(ctx.db, input.projectId);
+            if (role.name !== Roles.ProjectManager) {
+                throw new TRPCError({
+                    code: "UNAUTHORIZED",
+                    message:
+                        "Only the project manager is allowed to delete the project",
+                });
+            }
+
+            const project = await ctx.db
+                .delete(Project)
+                .where(eq(Project.id, input.projectId));
         }),
 } satisfies TRPCRouterRecord;
