9262389bb47adb6d1e455ba46297a460dc258459
Jose Sanchez <engineer@copydataai.com>
2024-07-24 19:42:22 +0100

    feat(nextjs): add DialogModifyProject

--- apps/nextjs/src/app/_components/ListProjects.tsx
+++ apps/nextjs/src/app/_components/ListProjects.tsx
@@ -10,6 +10,7 @@ import { Button } from "@acme/ui/button";
 import { Input } from "@acme/ui/input";
 import { toast } from "@acme/ui/sonner";
 
+import { DialogModifyProject } from "~/app/_components/DialogModifyProject";
 import { DropDownOptions } from "~/app/_components/DropDownOptions";
 import { api } from "~/trpc/react";
 
@@ -68,7 +69,15 @@ export function ListProjects({
               </p>
             </div>
 
-            <DropDownOptions projectId={project.id} role={project.role} />
+            <div className="flex items-center space-x-2">
+              <DialogModifyProject
+                projectId={project.id}
+                name={project.name}
+                description={project.description}
+              />
+
+              <DropDownOptions projectId={project.id} role={project.role} />
+            </div>
           </div>
         </div>
       ))}
