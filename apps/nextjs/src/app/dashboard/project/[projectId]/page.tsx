fe53efd870458d2e72095f00c2f661a40c528853
Jose Sanchez <engineer@copydataai.com>
2024-07-24 23:36:43 +0100

    feat(nextjs): add edit on projectId page

--- apps/nextjs/src/app/dashboard/project/[projectId]/page.tsx
+++ apps/nextjs/src/app/dashboard/project/[projectId]/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { Roles } from "@acme/db/schema";
 import { LoaderCircleLucide } from "@acme/ui/loader-circle";
 import { ScrollArea } from "@acme/ui/scroll-area";
 import { toast } from "@acme/ui/sonner";
 
 import { DialogFile } from "~/app/_components/DialogFile";
+import { DialogModifyProject } from "~/app/_components/DialogModifyProject";
 import { FilesTable } from "~/app/_components/FilesTable";
 import { api } from "~/trpc/react";
 
@@ -39,11 +41,25 @@ export default function ProjectIdPage({
     );
   }
 
+  console.log(data);
   return (
     <main className="flex flex-col items-center justify-center gap-4 py-4">
       <section className="flex flex-col items-center justify-center gap-4">
-        <div className="flex w-1/2 flex-col items-center justify-center">
-          <h1 className="text-2xl font-bold">{data?.name}</h1>
+        <div className="flex w-1/3 flex-col items-center justify-center">
+          <div className="flex w-full items-center justify-between">
+            <h1 className="text-2xl font-bold">{data?.name}</h1>
+
+            {(data?.role === Roles.ProjectManager ||
+              data?.role === Roles.Admin) && (
+              <div className="flex items-center">
+                <DialogModifyProject
+                  projectId={data?.id}
+                  name={data?.name}
+                  description={data?.description}
+                />
+              </div>
+            )}
+          </div>
           <ScrollArea className="h-[200px] w-[350px] rounded-md border p-4">
             {data?.description}
           </ScrollArea>
