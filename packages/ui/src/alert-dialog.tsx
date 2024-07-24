759b89f1bfce8ac701dde6a1581535abc87b5d85
Jose Sanchez <engineer@copydataai.com>
2024-07-24 17:44:05 +0100

    fix(ui): throtting import

--- packages/ui/src/alert-dialog.tsx
+++ packages/ui/src/alert-dialog.tsx
@@ -2,9 +2,9 @@
 
 import * as React from "react";
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
-import { buttonVariants } from "src//button";
 
 import { cn } from "@acme/ui";
+import { buttonVariants } from "@acme/ui/button";
 
 const AlertDialog = AlertDialogPrimitive.Root;
 
