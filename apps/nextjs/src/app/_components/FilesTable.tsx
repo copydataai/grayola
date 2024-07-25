5f30821d04490d8fe3137d8fdd125212a4fe7782
Jose Sanchez <engineer@copydataai.com>
2024-07-25 16:43:54 +0100

    feat(FilesTable): add DropDownFile

--- apps/nextjs/src/app/_components/FilesTable.tsx
+++ apps/nextjs/src/app/_components/FilesTable.tsx
@@ -6,27 +6,10 @@ import { Eye, MoreVertical, Pencil, Trash } from "lucide-react";
 import { Roles } from "@acme/db/schema";
 import { Button } from "@acme/ui/button";
 import { DataTable } from "@acme/ui/data-table";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@acme/ui/dialog";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@acme/ui/dropdown-menu";
 import { LoaderCircleLucide } from "@acme/ui/loader-circle";
 import { toast } from "@acme/ui/sonner";
 
+import { DropDownFile } from "~/app/_components/DropDownFile";
 import { GetSignedURL } from "~/app/dashboard/project/[projectId]/actions";
 import { api } from "~/trpc/react";
 
@@ -60,53 +43,14 @@ export const columns: ColumnDef<Files>[] = [
   {
     id: "actions",
     cell: ({ row }) => {
-      const router = useRouter();
-      const role = row.original.role;
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              size="icon"
-              variant="outline"
-              className="h-8 w-8 hover:bg-muted"
-            >
-              <MoreVertical className="h-3.5 w-3.5" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem
-              onSelect={async () => {
-                const { data, error } = await GetSignedURL(row.original.path);
-                if (error) {
-                  toast.error(error.message);
-                }
-                if (data) {
-                  toast.success("File downloaded");
-                  await router.push(data.signedUrl);
-                }
-              }}
-            >
-              <Eye className="mr-2 h-3.5 w-3.5" />
-              view
-            </DropdownMenuItem>
-            {(role === Roles.ProjectManager || role === Roles.Admin) && (
-              <>
-                <DropdownMenuItem onSelect={() => console.log("update")}>
-                  <Pencil className="mr-2 h-3.5 w-3.5" />
-                  update
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem
-                  onSelect={() => console.log("delete")}
-                  className="cursor-pointer text-destructive"
-                >
-                  <Trash className="mr-2 h-3.5 w-3.5" />
-                  Delete
-                </DropdownMenuItem>
-              </>
-            )}
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <DropDownFile
+          fileId={row.original.id}
+          projectId={row.original.projectId}
+          role={row.original.role}
+          createdAt={row.original.createdAt}
+          updatedAt={row.original.updatedAt}
+        />
       );
     },
   },
